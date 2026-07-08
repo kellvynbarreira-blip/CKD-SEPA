c2174ab11f2658e80419e001a3317bfddab3eb62
kellvynbarreira-blip <kellvyn.barreira@gmail.com>
2026-07-08 22:49:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ function generateQR() {
     const ratio = logoW / logo.width;
     const logoH = logo.height * ratio;
 
-    const circleD = Math.max(logoW, logoH) * 1.78;
+    const circleD = Math.max(logoW, logoH) * 1.60;
     const cx = qrW / 2;
     const cy = qrW / 2;
 
